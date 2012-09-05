c5dc75e6d10f0a407c025afaeff3367f5d18d925
Ryan Brainard <brainard@heroku.com>
2012-09-04 21:11:23 -0700

    init

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * @author Ryan Brainard
+ */
+public class SampleTest {
+
+    @Test
+    public void testName() throws Exception {
+        assertEquals(2, 1 + 1);
+    }
+}
